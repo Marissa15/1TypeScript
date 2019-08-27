a23054b9eadcf773d8392dbef34c19ce6eb8fbbf
marissa <asiram.1998@gmail.com>
2019-08-27 16:53:47 -0600

    Ejercicio9

---
+++ person.js
@@ -0,0 +1,15 @@
+var Person = /** @class */ (function () {
+    function Person(theName) {
+        this.name = theName;
+    }
+    Person.prototype.introduceSelf = function () {
+        console.log("Hi, I am " + this.name + "!");
+    };
+    return Person;
+}());
+var personA = new Person("Sally");
+// Prints "Hi, I am Sally!"
+personA.introduceSelf();
+personA.name = "Mindy";
+// Prints "Hi, I am Mindy!"
+personA.introduceSelf();
---
+++ person.ts
@@ -0,0 +1,20 @@
+class Person {
+    name: string;
+    constructor(theName: string) {
+        this.name = theName;
+    }
+    introduceSelf() {
+        console.log("Hi, I am " + this.name + "!");
+    }
+}
+ 
+let personA = new Person("Sally");
+ 
+// Prints "Hi, I am Sally!"
+personA.introduceSelf();
+ 
+personA.name = "Mindy";
+ 
+// Prints "Hi, I am Mindy!"
+personA.introduceSelf();
+
