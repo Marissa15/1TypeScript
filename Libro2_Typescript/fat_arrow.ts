d310a707eb9a05cb53c8cf090bfc6341f1876e5d
marissa <asiram.1998@gmail.com>
2019-08-29 22:28:40 -0600

    20

--- Libro2_Typescript/fat_arrow.ts
+++ Libro2_Typescript/fat_arrow.ts
@@ -1,11 +1,21 @@
-function Person(age) {
+function Person1(age) {
     this.age = age
     // aquí el this es el objeto y no quien hace la llamada
     this.growOld = () => {
         this.age++;
     }
   }
-  var person = new Person(1);
-  setTimeout(person.growOld,1000);
-  setTimeout(function() { console.log(person.age); },2000); //devuelve 2
+  var person1 = new Person1(1);
+  setTimeout(person1.growOld,1000);
+  setTimeout(function() { console.log(person1.age); },2000); //devuelve 2
+  
+  class Person2 {
+    constructor(public age:number) {}
+    growOld = () => {
+        this.age++;
+    }
+  }
+  var person2 = new Person2(1);
+  setTimeout(person2.growOld,1000);
+  setTimeout(function() { console.log(person2.age); },2000); // 2
   
