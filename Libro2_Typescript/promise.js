9b7ee7539345155015fabe523a136055a5596713
marissa <asiram.1998@gmail.com>
2019-08-29 22:40:59 -0600

    25

---
+++ Libro2_Typescript/promise.js
@@ -0,0 +1,27 @@
+//Ejemplo de promise (manejando los errores añadiendole un método catch a la cadena)
+Promise.reject(new Error('Algo malo ha pasado'))
+    .then(function (res) {
+    console.log(res); // no se llama
+    return 456;
+})
+    .then(function (res) {
+    console.log(res); // no se llama
+    return Promise.resolve(123);
+})
+    .then(function (res) {
+    console.log(res); // no se llama
+    return Promise.resolve(123);
+})["catch"](function (err) {
+    console.log(err.message); // Algo malo ha pasado        
+});
+Promise.resolve(123)
+    .then(function (res) {
+    throw new Error('something bad happened');
+    return 456;
+})
+    .then(function (res) {
+    console.log(res); // never called
+    return Promise.resolve(789);
+})["catch"](function (err) {
+    console.log(err.message); // something bad happened
+});
