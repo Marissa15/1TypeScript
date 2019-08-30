77b1409b9656399c8f7f92dc79800e63c7d50519
marissa <asiram.1998@gmail.com>
2019-08-29 22:33:19 -0600

    22

--- Libro2_Typescript/desestructuracion.ts
+++ Libro2_Typescript/desestructuracion.ts
@@ -12,3 +12,9 @@ console.log(array[0]); // 1
 var [x, y, z] = array;
 console.log(x); // 1
 
+// Desestructuración de arrays con estructuración
+
+var array = [1, 2, 3, 4];
+var [x, y, ...rest] = array;
+console.log(x, y, rest); // 1, 2, [3,4]
+
