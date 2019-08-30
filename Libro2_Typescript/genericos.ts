f8cb1c05c3052d3e8555a09ebcf9671633785aba
marissa <asiram.1998@gmail.com>
2019-08-29 20:27:48 -0600

    13

--- Libro2_Typescript/genericos.ts
+++ Libro2_Typescript/genericos.ts
@@ -26,8 +26,26 @@ function echo(arg) {
     return arg;
 }
 
-var a = echo("aaa");
-var t = echo({length: 2, name: "aa"});
+//var a = echo("aaa");
+//var t = echo({length: 2, name: "aa"});
 
 //esto no funciona
 //var b = echo(1);
+
+//function copyFields<T extends U, U>(source: T, target: U) : U {
+  //  for(let id in source){
+    //    if(target[id] != undefined){
+        //    source[id] = target[id];
+      //  }else {
+          //  target[id] = source[id];
+      //  }
+   // }
+
+    //return target;
+//}
+
+//let a = {a: 1, b: 2, c: 3};
+//let b = copyFields (a, {b: 10, c:20}); // Esto funcionará
+//let c = copyFields (a, {Q: 20}); // Esto NO funcionará
+//console.log(b); // 1, 10, 20
+//console.log(c); // Lo devuelve si lo compilas a pesar de saber que está mal
