66d9a8728202e53619aae7cc035371f4697d534a
marissa <asiram.1998@gmail.com>
2019-09-01 17:15:58 -0600

    26

---
+++ Libro2_Typescript/JSON.ts
@@ -0,0 +1,12 @@
+function iReturnPromiseAfter1Second():Promise<string> {
+    return new Promise((resolve)=>{
+        setTimeout(()=>resolve("Hello world!"), 1000);
+    });
+}
+Promise.resolve(123)
+    .then((res)=>{
+         return iReturnPromiseAfter1Second();
+    })
+    .then((res) => {
+        console.log(res); // HOLA MUNDO!!
+    });
