87a1e47b90a18b651916aa154dc37c2f55b0b1f1
marissa <asiram.1998@gmail.com>
2019-09-10 17:03:21 -0600

    directivas_bien

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {fecha_actualComponent} from './fecha_actual/fecha_actual.component';
 import { CopyrightComponent } from './copyright/copyright.component';
+import { EjdirectivangifComponent } from './ejdirectivangif/ejdirectivangif.component';
+import { ViewmodeloComponent } from './viewmodelo/viewmodelo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     fecha_actualComponent,
-    CopyrightComponent
+    CopyrightComponent,
+    EjdirectivangifComponent,
+    ViewmodeloComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, FormsModule,
+    AppRoutingModule, BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
